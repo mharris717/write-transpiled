174285b817e5be4ebbaf1ed6381636fd2199911f
Mike Harris <mharris717@gmail.com>
2014-10-27 16:50:50 -0400

    CS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+var setupGrunt = function(grunt) {
   grunt.loadNpmTasks('grunt-broccoli')
   grunt.initConfig({
     broccoli: {
@@ -8,4 +8,56 @@ module.exports = function(grunt) {
       }
     }
   })
+};
+
+if (true) {
+  var makeTree = require("./lib/make-tree")
+  var brocProg = require("./broc_prog")
+
+  var sup = {
+    name: "wt:hello",
+
+    run: function() {
+      console.log("hello from write-transpiled");
+      //return brocProg('app')
+    }
+  };
+
+  var write = {
+    name: "write-transpiled",
+
+    run: function() {
+      
+    }
+  }
+
+  // var big = {
+  //   name: 'cordova:build',
+  //   aliases: ['cdv:build'],
+  //   description: 'Build the ember and cordova project together running in the simulator or on a device',
+  //   works: 'insideProject',
+
+  //   availableOptions: [
+  //     { name: 'environment', type: String, default: 'development' },
+  //     { name: 'platform', type: String, default: 'ios' }
+  //   ],
+
+  //   run: function() {
+  //     console.log("cordova run");
+  //   }
+  // };
+
+  module.exports = {
+    name: "write-transpiled", 
+
+    included: function() {
+      console.log("in write-transpiled included")
+    },
+
+    includedCommands: function() {
+      return [sup]
+    },
+
+    setupGrunt: setupGrunt
+  }
 }
